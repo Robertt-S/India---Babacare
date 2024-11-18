a2825f282465c5a8f54636f3d20366cd0a76a25e
GuilhermeFerracini <guilherme.almeida.ferracini@gmail.com>
2024-11-18 17:33:26 -0300

    CRUD admin e models

--- BabaCare/users/admin.py
+++ BabaCare/users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import Baba, Responsavel
+
+class ListandoBabas(admin.ModelAdmin):
+    list_display = ("id", "nome", "email", "verificado")
+    list_display_links = ("id","nome")
+    search_fields = ("nome",)
+    list_filter = ("id",)
+    list_editable = ("verificado",)
+    list_per_page = 10
+
+admin.site.register(Baba, ListandoBabas)
+
+
+class ListandoResponsaveis(admin.ModelAdmin):
+    list_display = ("id", "nome", "email", "verificado")
+    list_display_links = ("id","nome")
+    search_fields = ("nome",)
+    list_filter = ("id",)
+    list_editable = ("verificado",)
+    list_per_page = 10
+
+admin.site.register(Responsavel, ListandoBabas)
--- BabaCare/users/models.py
+++ BabaCare/users/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
-# Create your models here.
+from datetime import date
+
+
+
+## Exemplo do GPT, deve ter algo melhor
+# from django.core.exceptions import ValidationError
+# def validar_cpf(cpf):
+#     # Implementar lógica de validação de CPF aqui.
+#     if len(cpf) != 11 or not cpf.isdigit():
+#         raise ValidationError("CPF inválido")
+
+
+
+
+class Baba(models.Model):
+
+    
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    cpf = models.CharField(max_length=11, unique=True)  
+    data_nascimento = models.DateField(null=True, blank=True)
+    email = models.EmailField(unique=True)  
+    senha = models.CharField(max_length=128)
+    numero_celular = models.CharField(max_length=15, unique=True)   
+    endereco = models.TextField()  
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    verificado = models.BooleanField(default=False)
+    
+
+    def calcular_idade(self):
+        if self.data_nascimento:
+            hoje = date.today()
+            return hoje.year - self.data_nascimento.year - (
+                (hoje.month, hoje.day) < (self.data_nascimento.month, self.data_nascimento.day)
+            )
+        return None
+
+
+    def __str__(self):
+        return self.nome
+    
+    
+    
+class Responsavel(models.Model):
+
+    
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    cpf = models.CharField(max_length=11, unique=True)  
+    data_nascimento = models.DateField(null=True, blank=True)
+    email = models.EmailField(unique=True)  
+    senha = models.CharField(max_length=128)
+    numero_celular = models.CharField(max_length=15, unique=True)   
+    endereco = models.TextField()  
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    verificado = models.BooleanField(default=False)
+    
+
+    def calcular_idade(self):
+        if self.data_nascimento:
+            hoje = date.today()
+            return hoje.year - self.data_nascimento.year - (
+                (hoje.month, hoje.day) < (self.data_nascimento.month, self.data_nascimento.day)
+            )
+        return None
+
+
+    def __str__(self):
+        return self.nome
