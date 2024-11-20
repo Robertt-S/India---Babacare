ab1bdf787521418afe7b7a257d1d62c7a7040585
Aline <alinenlmoura@gmail.com>
2024-11-20 00:58:31 -0300

    fluxo login/cadastro pt1

--- BabaCare/setup/urls.py
+++ BabaCare/setup/urls.py
@@ -20,6 +20,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from users import views as v
 
 app_name = 'main'
 
@@ -32,6 +33,8 @@ urlpatterns = [
     #path('users/',include('django.contrib.auth.urls')),
     path('users/',include('users.urls')),
     path('perfis/',include('perfis.urls')),
+    path('login/', v.login_view, name='login'), 
+    path('cadastro/', v.register_user, name='cadastro'),  
 ]
 
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
