fa3d7be2b9f3dbd448c49b88bef7a5c7165f4828
Caputino <10758372+capuano1@users.noreply.github.com>
2025-01-25 18:39:49 -0300

    Changed Feedback from Forms to ModelForms

--- BabaCare/feedback/forms.py
+++ BabaCare/feedback/forms.py
@@ -1,37 +1,16 @@
 from django import forms
+from django.forms import ModelForm
+from .models import Feedback
 
-class FeedbackForms (forms.Form):
-    forms_title=forms.CharField(
-        label="Título da Avaliação",
-        required=True,
-        max_length=100,
-        widget=forms.CharField(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Título da avaliação'
-            }
-        )
-    )
-    forms_score=forms.IntegerField(
-        label="Nota da Avaliação",
-        required=True,
-        min_value=0,
-        max_value=10,
-        widget=forms.TextInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Nota'
-            }
-        )
-    )
-    forms_description=forms.CharField(
-        label="Descrição da Avaliação",
-        required=True,
-        max_length=500,
-        widget=forms.TextInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Sua avaliação'
-            }
-        )
-    )
+class FeedbackForms (ModelForm):
+    
+    score = forms.IntegerField(required=True, min_value=0, max_value=10, widget=forms.TextInput, label="Nota")
+
+    class Meta:
+        model = Feedback
+        fields = ["title", "score", "description"]
+        labels={
+            "title": "Título da Avaliação",
+            "score": "Nota",
+            "description": "Avaliação"
+        }
