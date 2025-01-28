386bde2988cf7d189b26d4853934b8a487c50dc7
EvandroKayano <ek.kayano@unifesp.br>
2025-01-27 22:25:14 -0300

    Avaliação da Baba corrigida

--- BabaCare/feedback/views.py
+++ BabaCare/feedback/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from perfis.models import Servico
 from .models import Feedback
 from .forms import FeedbackForms
+from django.contrib import messages
 
 def feedback_page(request, contract):
     feedback = Feedback.objects.get(contract=contract)
@@ -27,11 +28,34 @@ def give_feedback(request, servico_id):
     
     return render(request, 'feedback/give_feedback_page.html', {'form': form, 'contract': contract})
 
+
 def edit_feedback(request, contract):
-    feedback = Servico.objects.get(id=contract)
-    form = FeedbackForms(data=(request.POST or None), instance = feedback)
-    if form.is_valid():
-        form.save()
-        return redirect('home')
     
-    return render(request, 'feedback/give_feedback_page.html', {'form': form, 'contract': contract})
+    servico = Servico.objects.filter(id=contract).first()
+    try:
+        feedback = Feedback.objects.filter(contract=servico).first()
+    except Feedback.DoesNotExist:
+        feedback = None
+    
+    #editar
+    if(feedback):
+        form = FeedbackForms(data=(request.POST or None), instance = feedback)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Avaliação editada!')
+            return redirect('users:servicos_responsavel')
+        
+        return render(request, 'feedback/give_feedback_page.html', {'form': form, 'contract': servico})
+    #primeira avaliação
+    else:
+        if request.method == 'POST':
+            form = FeedbackForms(data=(request.POST or None))
+            if form.is_valid():
+                aux = form.save(commit=False)
+                aux.contract = servico
+                form.save()
+                return redirect('home')
+        else:
+            form = FeedbackForms()
+        
+    return render(request, 'feedback/give_feedback_page.html', {'form': form, 'contract': servico})
