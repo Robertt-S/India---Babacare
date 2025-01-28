3d6a4b946083dbd66160222d7efc03015ff4a215
EvandroKayano <ek.kayano@unifesp.br>
2025-01-27 21:51:21 -0300

    Correção da avaliação

--- BabaCare/feedback/models.py
+++ BabaCare/feedback/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from users.models import Servico
+from perfis.models import Servico
 
 class Feedback(models.Model):
     contract = models.ForeignKey(Servico, on_delete=models.CASCADE)
